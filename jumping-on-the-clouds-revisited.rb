cddc3b303b68a9c4972fe5c9bb25ccff7bbc4ff7
Maulidan Nashuha <maulidanms@gmail.com>
2020-10-09 10:12:03 +0700

    jumping on the clouds-revisited better implementation

--- jumping-on-the-clouds-revisited.rb
+++ jumping-on-the-clouds-revisited.rb
@@ -2,9 +2,8 @@ def jumpingOnClouds(c, k)
     e = 100
     i = 0
     loop do
-        v = c[i]
-        e = decreaseEnergy(e, v, i, k)
-        i = nextCloud(i, k, c)
+        e = decreaseEnergy(e, c[i], k)
+        i = nextCloud(i, k, c.length)
 
         break if i == 0
     end
@@ -12,23 +11,12 @@ def jumpingOnClouds(c, k)
     return e
 end
 
-def decreaseEnergy(e, v, i, k)
-    e -= 1
-
-    if v > 0
-        e -= 2
-    end
-
-    return e
+def decreaseEnergy(e, v, k)
+    return e - (v * 2 + 1)
 end
 
-def nextCloud(i, k, c)
-    i += k
-    if i >= c.length
-        i = i - c.length
-    end
-    
-    return i
+def nextCloud(i, k, n)
+    return (i + k) % n
 end
 
 nk = gets.rstrip.split
